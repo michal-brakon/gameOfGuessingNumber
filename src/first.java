a44e554aaf715fb31b70874d4a26985567b8588b
Michal <zielonydeveloper@o2.pl>
2019-11-13 13:39:52 +0100

    game of guessing the number is working

--- src/first.java
+++ src/first.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
 public class first {
+    public static void main(String[] args){
+        System.out.println("Enter your address: ");
+        Scanner scanner = new Scanner(System.in);
+        String address = scanner.nextLine();
+        System.out.println("You live at: " + address);
+        System.out.println("Whats Your name bitch?");
+        Scanner scanner2 = new Scanner(System.in);
+        String name = scanner2.nextLine();
+        System.out.println("NO! You are just BITCH!");
+
+
+    }
 }
---
+++ src/second.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class second {
+    public static void main(String[] args) {
+        double number = Math.random();
+        double xNumber = number * 100 + 1;
+        int numberToGuess = (int) xNumber; // liczba do zgadniecia to 1-100
+        int numberOfGuesses = 10;
+
+        System.out.println("I have randomly chosen a number between 1 and 100");
+        System.out.println("Try to guess it");
+
+
+        for (int i = numberOfGuesses; i > 0; i--) {
+            System.out.println("You have " + i + " guess(es) left");
+            System.out.println("Choose number:");
+            Scanner scanner = new Scanner(System.in);
+            int userNumber = scanner.nextInt();
+
+            if (userNumber == numberToGuess) {
+                System.out.println("Correct! You win");
+                break;
+            } else if (userNumber > numberToGuess) {
+                System.out.println("Number is smaller");
+            } else if (userNumber < numberToGuess) {
+                System.out.println("Number is bigger");}
+
+        }
+        System.out.println("You lost");
+        System.out.println("The number was " + numberToGuess);
+        }
+    }
+
+
+
+
+
+
