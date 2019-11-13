51908b91a98e97bd932dc4074407e9a5ef606c3e
Michal <zielonydeveloper@o2.pl>
2019-11-13 14:13:29 +0100

    corrected game, now win and lose comminicats wors=ks proper

--- src/second.java
+++ src/second.java
@@ -5,7 +5,7 @@ public class second {
         double xNumber = number * 100 + 1;
         int numberToGuess = (int) xNumber; // liczba do zgadniecia to 1-100
         int numberOfGuesses = 10;
-
+        boolean win = false;
         System.out.println("I have randomly chosen a number between 1 and 100");
         System.out.println("Try to guess it");
 
@@ -15,20 +15,26 @@ public class second {
             System.out.println("Choose number:");
             Scanner scanner = new Scanner(System.in);
             int userNumber = scanner.nextInt();
-
             if (userNumber == numberToGuess) {
-                System.out.println("Correct! You win");
+                win = true;
                 break;
             } else if (userNumber > numberToGuess) {
-                System.out.println("Number is smaller");
-            } else if (userNumber < numberToGuess) {
+                System.out.println("Number is smaller"); }
+            else if (userNumber < numberToGuess) {
                 System.out.println("Number is bigger");}
+        }
+            if(win) {
+                System.out.println("Correct! You win, the number was " + numberToGuess); }
+        else  {
+            System.out.println("You lost");
+                System.out.println("The number was " + numberToGuess);}
+
+
+
 
         }
-        System.out.println("You lost");
-        System.out.println("The number was " + numberToGuess);
-        }
     }
+
 
 
 
