e249e403647477b9e1fe0a2dae2dffa616a9d8e5
Michal <zielonydeveloper@o2.pl>
2019-11-13 15:06:14 +0100

    program that counts words in file

--- src/WwordCount.java
+++ src/WwordCount.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class WwordCount {
-}
+    public static void main(String[] args) throws Exception { // wyrzuca potencjalne wyjątki
+        File myBook = new File("1342-0.txt"); //dodany obiekt myBook i podana jego sciezka
+        Scanner nazwaScannera = new Scanner(myBook); // scanner ma skanowac plik- mybook
+        int words = 0; //początkowa wartosc słów
+        while(nazwaScannera.hasNext()); {// loop ktory sprawdza czy jest kolejna linia w pliku
+            String line = nazwaScannera.nextLine(); // sprawdzamy co jest w linii
+            words += line.split(" ").length; //dodaje do words liczbe znaków odzielonych spacją (słow) do siebie
+
+/* cały loop
+sprawdza linijka po linicje ilosć słow i dodaje do siebie
+ */
+        }
+            System.out.println("myBook contains: " + words + "words."); //wysywietla sume slow w książce
+
+
+
+
+
+
+    }
+   }
+
